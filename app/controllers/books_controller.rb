a1a582a586d4ccbd4380bc9885c47e1e787ca5f5
LilyTTT <yushantang4@gmail.com>
2024-01-28 12:49:09 -0600

    Completed all views (and flash notice)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,28 +1,57 @@
 class BooksController < ApplicationController
   def index
     @page = params[:page].to_i
-    @tasks = Task.order(:id)
+    @books = Book.order(:id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+    
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save 
+      flash[:notice] = "Book was successfully added."
+      redirect_to books_path
+    else
+      render('new')
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully deleted."
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :price, :published_date)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "main#index"
+  root "books#index"
 
   get 'main/index'
 
