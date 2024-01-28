99e04cac5c0d34aa6d3947cdbe0a642b7ba6ead3
LilyTTT <yushantang4@gmail.com>
2024-01-28 11:48:43 -0600

    Compeleted db setup

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+  def index
+    @page = params[:page].to_i
+    @tasks = Task.order(:id)
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  root "main#index"
+
+  get 'main/index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
+
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
